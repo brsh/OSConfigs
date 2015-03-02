1c8e00c58ec3f09cb132b0f644dbb96930ea34d7
Brian Sheaffer <brsh@users.noreply.github.com>
2015-03-02 09:59:49 -0800

    Cleaned up the ansicon test a little
    Test for ansicon and load it. If it loaded, test for the Ver variable and make use of it...

--- win-cmd/auto.bat
+++ win-cmd/auto.bat
@@ -10,20 +10,25 @@ doskey mv=move $*
 doskey cp=copy $*
 doskey rm=del $*
 
+::Load Ansicon if it exists - the -p connects it to "this" instance of cmd
+if EXIST %WINDIR%\ansicon.exe (
+	"C:\Windows\ansicon" -p
+)
+
 ::Tests if running as admin 
-::(needs a command that a reg user can't run; at will be replaced someday...)
+::(needs a command that a reg user can't run)
 %SystemRoot%\system32\net.exe session >nul 2>&1
 IF %errorlevel% == 0 (
 	:: We are admin
 	set IsAdmin=$Sas$SAdmin
-	if EXIST %WINDIR%\ansicon.exe (
+	if DEFINED ANSICON_VER (
 		set IsAdmin=$E[1;31m$Sas$SAdmin
 	)
 )
 
 ::Set the prompt
 ::The time portion ($T) includes miliseconds and seconds - $H backspaces them out
-if EXIST %WINDIR%\ansicon.exe (
+if DEFINED ANSICON_VER (
 	prompt $e[1;37m[$e[1;33m$T$H$H$H$H$H$H$e[1;37m]$S[$e[1;32m%username%%IsAdmin%$e[1;37m]$S[$e[1;36m$P$e[1;37m]$_$G$e[0m$s
 ) ELSE (
 	prompt [$T$H$H$H$H$H$H]$S[%username%%IsAdmin%]$S[$P]$_$G$S
