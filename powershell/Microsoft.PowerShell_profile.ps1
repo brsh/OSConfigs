055198a7f3ffbfccc1bb21e75c87111feb8144b2
Brian Sheaffer <brsh@users.noreply.github.com>
2016-06-29 13:59:15 -0700

    Start of Libs folder; added Path Manipulations
    I'm working on creating a fully fledged library folder structure. I've
    moved my modules out of PSHome and will eventually move the profile.
    
    With that, I've added Add-ToPath and Remove-FromPath functions -
    complete with a config ini (for the remove). More on that soon.

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -37,7 +37,7 @@ function AddPSDefault([string]$name, $value) {
     }
 }
 
-function Import-MyModules {
+function Import-GeneralModules {
     param (
         [Parameter(Position=0, Mandatory=$true)]
         [String] $Name
@@ -1013,6 +1013,102 @@ function Get-CurrentCalendar {
 
 set-alias -name curcal -value Get-CurrentCalendar -Description "Show last, current, and next months" -Force
 
+function Add-ToPath {
+<#
+    .SYNOPSIS
+    Adds directory to the path
+    .DESCRIPTION
+    Adds a directory (or directories) to the path for the current (or child) instance(s) of PowerShell. The script will take pipeline info and will verify that the directory exists, is not already in the path, and is actually a directory (dropping any file name and extension).
+    .PARAMETER Directories (Directory, Dir, Folder, ProviderPath)
+    The folder/directory to add to the path
+    .EXAMPLE
+    Add-ToPath -folder c:\scripts\lib
+    .EXAMPLE
+    ".", "C:\Scripts\lib" | Add-ToPath
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$True, ValueFromPipeLineByPropertyName=$True, HelpMessage="What folder would you like to add?")]
+        [Alias('dir','folder','directory','providerpath')]
+        [string[]] $Directories
+    )
+    
+    BEGIN { 
+        $CurPath = $env:Path.Split(';')
+    }
+
+    PROCESS {
+        ForEach ($Directory in $Directories) {
+            $Dir = ""
+            Switch ($Directory) {
+                { test-path $_ -PathType Container } { $Dir = (Resolve-Path $_).ProviderPath; break }
+                { test-path $_ -PathType leaf } { $Dir = (Resolve-Path (Split-Path $_ -Parent)).ProviderPath; break }
+            }
+
+            if ($Dir.Length -gt 0) {
+                if ($CurPath -contains $Dir) {
+                    Write-Verbose "$Dir already exists in the path"
+                }
+                else {
+                    $CurPath += $Dir
+                    Write-Verbose "Added $Dir to path"
+                }
+            }
+            else { Write-Verbose "$Directory not added to path - it is not a valid directory" }
+        }
+    }
+
+    END {
+        $env:Path = [String]::Join(';', $CurPath)
+    }
+}
+
+Set-Alias -name PathAdd -Value Add-ToPath -Description "Adds a directory to the path" -Force
+
+function Remove-FromPath {
+<#
+    .SYNOPSIS
+    Removes a directory from the path
+    .DESCRIPTION
+    Removes a directory (or directories) from the path for the current (or child) instance(s) of PowerShell. The script will take pipeline info and will verify that the directory is already in the path.
+    .PARAMETER Directories (Directory, Dir, Folder, ProviderPath)
+    The folder/directory to remove from the path
+    .EXAMPLE
+    Remove-FromPath -folder c:\scripts\lib
+    .EXAMPLE
+    ".", "C:\Scripts\lib" | Remove-FromPath
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$True, ValueFromPipeLineByPropertyName=$True, HelpMessage="What folder would you like to remove?", Position=0)]
+        [Alias('dir','folder','directory','providerpath')]
+        [string[]] $Directories,
+        [Parameter(Mandatory=$false, Position=1)]
+        [Alias('show', 'diff')]
+        [Switch] $ShowDifference = $false
+    )
+    
+    BEGIN { 
+        $CurPath = ";${env:Path};"
+        if ($ShowDifference) { "--Before--"; Get-SplitEnvPath } 
+    }
+
+    PROCESS {
+        ForEach ($Directory in $Directories) {
+            $CurPath = $CurPath -iReplace [regex]::Escape(";${Directory};"), ";"
+            Write-Verbose "Tried to remove $Directory from path"
+        }
+    }
+
+    END {
+        $CurPath = $CurPath.Split(';',[StringSplitOptions]::RemoveEmptyEntries)
+        $env:Path = [String]::Join(';', $CurPath)
+        if ($ShowDifference) { "--After--"; Get-SplitEnvPath } 
+    }
+}
+
+Set-Alias -name PathDel -Value Remove-FromPath -Description "Removes a directory from the path" -Force
+
 
 #####################  Actual Work  #####################
 
@@ -1020,10 +1116,16 @@ set-alias -name curcal -value Get-CurrentCalendar -Description "Show last, curre
 Set-PSReadLineOption –HistoryNoDuplicates:$True
 
 #Modules
-Import-MyModules Directories
-Import-MyModules psSysInfo
-Import-MyModules psOutput
-Import-MyModules psPrompt
+if (test-path C:\Scripts\lib\Modules) {
+    #I recommend:
+    #   changing the owner of the folder to "Administrators" or "Administrator"
+    #   and removing read/write from general users (like 'Authenticated Users' or 'Everyone'... or your own account)
+    #Just to restrict changes a little - just a little...
+    Get-ChildItem C:\Scripts\lib\Modules *.psm1 -Recurse | ForEach-Object { Import-Module $_.FullName -force }
+}
+else {
+    #These assume the modules are stored in the "standard" locations
+}
 
 #PSDrives
 New-ProfilePSDrive -name Profile -Location $env:USERPROFILE -Description "Home Directory"
@@ -1031,6 +1133,12 @@ New-ProfilePSDrive -name Documents -Location $env:USERPROFILE\Documents -Descrip
 New-ProfilePSDrive -name Downloads -Location $env:USERPROFILE\Downloads -Description "User Downloads folder"
 New-ProfilePSDrive -name GitHub -Location $env:USERPROFILE\Documents\GitHub -Description "Git master directories"
 New-ProfilePSDrive -name PSHome -Location $PSHome -Description "Powershell program folder"
+
+#Path Adjustments
+Add-ToPath -folder "C:\scripts\lib.ps"
+if (Test-Path C:\Scripts\lib.ps\Settings\remove-frompath.ini) {
+    get-content C:\Scripts\lib.ps\Settings\remove-frompath.ini | Remove-FromPath
+}
 
 #Defaults
 AddPSDefault "Format-Table:AutoSize" {if ($host.Name -eq 'ConsoleHost'){$true}}
